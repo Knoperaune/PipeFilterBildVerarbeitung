4f35e1a55806efb0212bbf5e3ed059c9a52eeb23
phil <phil@172.16.63.188>
2012-11-29 10:16:34 +0100

    function for hubert

--- PipeFilterBild/src/PicturePAF/Processor/CentroidProcessor.java
+++ PipeFilterBild/src/PicturePAF/Processor/CentroidProcessor.java
@@ -9,34 +9,27 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
-import java.awt.image.renderable.ParameterBlock;
 import java.util.LinkedList;
-import javax.media.jai.JAI;
-import javax.media.jai.ParameterBlockJAI;
 import javax.media.jai.PlanarImage;
-import javax.media.jai.ROI;
-import javax.media.jai.RenderedOp;
-import javax.media.jai.registry.RenderedRegistryMode;
 
 /**
+ * Thanks Michael Faißt & Florian Fink for the help
  *
  * @author phil
  */
 public class CentroidProcessor extends GenericProcessor<PlanarImage, PlanarImage> {
 
     LinkedList<PlanarImage> rois;
-    LinkedList<Rectangle> roisRectangles;
+    LinkedList<Rectangle> roisRect;
     LinkedList<Point> centroids;
-    int maxBrightness = 244;
-    int ballcount = 7;
+    int white = 244;
+    int balls = 7;
 
     public CentroidProcessor() {
         rois = new LinkedList<>();
-        roisRectangles = new LinkedList<>();
+        roisRect = new LinkedList<>();
         centroids = new LinkedList<>();
     }
 
@@ -55,25 +48,49 @@ public class CentroidProcessor extends GenericProcessor<PlanarImage, PlanarImage
          pbj.setParameter("roi", roi);
          RenderedOp asdf = JAI.create("centroid", pbj);
          addOutput(image);*/
-       /* ParameterBlock pb = new ParameterBlock();
-        ROI roi = new ROI(JAI.create("bandselect", image, new int[]{0}));
-        pb.add(roi);
-        pb.addSource(image);
+        /* ParameterBlock pb = new ParameterBlock();
+         ROI roi = new ROI(JAI.create("bandselect", image, new int[]{0}));
+         pb.add(roi);
+         pb.addSource(image);
 
-        PlanarImage centroidImage = JAI.create("centroid", pb, RenderingHints.KEY_ANTIALIASING);
-        addOutput(centroidImage);*/
-        
-        
-        
-        for (int i = 0; i < ballcount; i++) {
+         PlanarImage centroidImage = JAI.create("centroid", pb, RenderingHints.KEY_ANTIALIASING);
+         addOutput(centroidImage);*/
+
+        getROIs(image);
+        createROIRecs();
+
+        BufferedImage buf = image.getAsBufferedImage();
+        Graphics2D g = buf.createGraphics();
+
+        for (Point p : centroids) {
+            g.setColor(Color.BLACK);
+            g.fillOval(p.x - 2, p.y - 2, 4, 4);
+        }
+
+        //JAI.create("filestore", buf, "centroid.jpg", "JPEG");
+
+        addOutput(PlanarImage.wrapRenderedImage(buf));
+    }
+
+    /**
+     * Cuts the ROIs out of the Image
+     *
+     * @param image
+     */
+    private void getROIs(PlanarImage image) {
+        for (int i = 0; i < balls; i++) {
             Rectangle temp = new Rectangle(i * 60, 10, 45, 50);
-            roisRectangles.add(temp);
+            roisRect.add(temp);
             PlanarImage tempImg = PlanarImage.wrapRenderedImage(
                     (RenderedImage) image.getAsBufferedImage(temp, image.getColorModel()));
             rois.add(tempImg);
-           // JAI.create("filestore", tempImg, "roi_loetstelle" + i + ".jpg", "JPEG");
         }
+    }
 
+    /**
+     * creates the Rectangles of the ROI
+     */
+    private void createROIRecs() {
         for (int i = 0; i < rois.size(); i++) {
 
             PlanarImage pi = rois.get(i);
@@ -88,7 +105,7 @@ public class CentroidProcessor extends GenericProcessor<PlanarImage, PlanarImage
                 for (int x = 0; x < pi.getWidth(); x++) {
                     int sample = pi.getData().getPixel(x, y, (int[]) null)[0];
 
-                    if (sample >= maxBrightness) {
+                    if (sample >= white) {
                         xWhite.add(x);
                     }
                 }
@@ -105,7 +122,7 @@ public class CentroidProcessor extends GenericProcessor<PlanarImage, PlanarImage
                 for (int y = 0; y < pi.getHeight(); y++) {
                     int sample = pi.getData().getPixel(x, y, (int[]) null)[0];
 
-                    if (sample >= maxBrightness) {
+                    if (sample >= white) {
                         yWhite.add(y);
                     }
                 }
@@ -115,21 +132,9 @@ public class CentroidProcessor extends GenericProcessor<PlanarImage, PlanarImage
                 }
             }
 
-            centroids.add(new Point(mittelwertX + roisRectangles.get(i).x,
-                    mittelwertY + roisRectangles.get(i).y));
+            centroids.add(new Point(mittelwertX + roisRect.get(i).x,
+                    mittelwertY + roisRect.get(i).y));
         }
-
-        BufferedImage buf = image.getAsBufferedImage();
-        Graphics2D g = buf.createGraphics();
-
-        for (Point p : centroids) {
-            g.setColor(Color.BLACK);
-            g.fillOval(p.x - 2, p.y - 2, 4, 4);
-        }
-
-        //JAI.create("filestore", buf, "centroid.jpg", "JPEG");
-        
-        addOutput(PlanarImage.wrapRenderedImage(buf));
     }
 
     @Override
